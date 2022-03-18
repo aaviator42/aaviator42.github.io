21545a0a5d49f2628eebc6c6b2377203d1153127
Aavi <83829272+aaviator42@users.noreply.github.com>
2022-03-18 09:01:04 +0530

    Update daaku.js

--- inc/js/daaku.js
+++ inc/js/daaku.js
@@ -1,28 +1,18 @@
-//modified from https://sanketmehta7.github.io/DarkMode-JS-BookMark/
+//modified from https://gist.github.com/lweiss01/7a6c60843b64236b018e7398fb0d5f40
 //check if user prefers dark mode
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-
-  //invert colors, skip transparency values
-  function invert(str, type) {
-    var t1 = str.split("(");
-    var t2 = t1[1].split(")");
-    var t3 = t2[0].split(",");
-    t3.forEach(function(v, i) {
-      if (i < 3) {
-        if (type == "color")
-          t3[i] = (255 - parseInt(v)) < 50 ? 120 : (255 - parseInt(v));
-        else
-          t3[i] = (255 - parseInt(v));
-      }
-    })
-    t3 = t3.join(",");
-    return t1[0] + "(" + t3 + ")";
-  }
-  //invert color and backgroundcolor of every dom node
-  document.querySelectorAll('*:not([invTouch])').forEach(function(node) {
-    var style = window.getComputedStyle(node);
-    node.style.backgroundColor = invert(style.backgroundColor, "back");
-    node.style.color = invert(style.color, "color");
-    node.setAttribute("invTouch", "true");
-  });
+  (d => {
+    var css = `:root{background-color:#f1f1f1;filter:invert(1) hue-rotate(180deg)}img:not([src*=".svg"]),picture,video{filter: invert(1) hue-rotate(180deg)}`,
+      style, id = "dark-mode",
+      ee = d.getElementById(id);
+    if (null != ee) ee.parentNode.removeChild(ee);
+    else {
+      style = d.createElement('style');
+      style.type = "text/css";
+      style.id = id;
+      if (style.styleSheet) style.styleSheet.cssText = css;
+      else style.appendChild(d.createTextNode(css));
+      (d.head || d.querySelector('head')).appendChild(style)
+    }
+  })(document)
 }
